d2d8359d3bac5fbbdc6a91222ea9eff9ead4ada4
mauricio  gallego <m.gallego.avila@accenture.com>
2020-08-03 18:14:50 -0300

    fetch

--- src/components/tabBriefcase.js
+++ src/components/tabBriefcase.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import foto1 from "../assets/imagen/foto1.png";
-import foto2 from "../assets/imagen/foto2.png";
-import foto3 from "../assets/imagen/foto3.png";
-import foto4 from "../assets/imagen/foto4.png";
-import foto5 from "../assets/imagen/foto5.png";
-import foto6 from "../assets/imagen/foto6.png";
-import foto7 from "../assets/imagen/foto7.png";
-import tabBriefcaseItem from '../components/tabBriefcase.item';
-
-
-const imgs = [foto1, foto2, foto3, foto4, foto5, foto6, foto7]
 
 class TabBriefcase extends Component {
     render() {
@@ -21,12 +10,14 @@ class TabBriefcase extends Component {
                     <Tab>Title 1</Tab>
                     <Tab>Title 2</Tab>
                 </TabList>
-            <tabBriefcaseItem imgs={imgs}></tabBriefcaseItem>
-            <tabBriefcaseItem imgs={imgs}></tabBriefcaseItem>
+                <TabPanel>
+                    {this.props.imgs.map(i => {
+                        return (<img src={i} alt='logo' />)
+                    })}
+                </TabPanel>
             </Tabs>
         )
     }
-
 }
 
 export default TabBriefcase;
